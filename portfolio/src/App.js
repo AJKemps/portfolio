138343ee9394532879c4eb2a298b4c838e5046e8
Alex Kemper <alex.j.kemper@gmail.com>
2020-11-09 20:10:19 -0500

    added https redirect provider

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,17 +3,20 @@ import Header from "./components/header";
 import About from "./components/about";
 import Projects from "./components/projects";
 import Footer from "./components/footer";
+import HttpsRedirect from "react-https-redirect";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-container">
-        <Header />
-        <About />
-        <Projects />
-        <Footer />
+    <HttpsRedirect>
+      <div className="App">
+        <div className="App-container">
+          <Header />
+          <About />
+          <Projects />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </HttpsRedirect>
   );
 }
 
